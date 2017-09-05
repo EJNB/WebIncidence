703391dfa8eda3569ce1d72b5a74b9a33a79f1db
Javier <javiernieto1989@gmail.com>
2017-09-05 10:33:13 -0400

    quite el formulario de deleteForm en el controlador

--- src/System/BackendBundle/Controller/SubCategoryController.php
+++ src/System/BackendBundle/Controller/SubCategoryController.php
@@ -42,7 +42,7 @@ class SubCategoryController extends Controller
             $em->persist($subCategory);
             $em->flush();
 
-            return $this->redirectToRoute('subcategory_show', array('id' => $subCategory->getId()));
+            return $this->redirectToRoute('subcategory_index', array('id' => $subCategory->getId()));
         }
 
         return $this->render('subcategory/new.html.twig', array(
@@ -71,20 +71,18 @@ class SubCategoryController extends Controller
      */
     public function editAction(Request $request, SubCategory $subCategory)
     {
-        $deleteForm = $this->createDeleteForm($subCategory);
         $editForm = $this->createForm('System\BackendBundle\Form\SubCategoryType', $subCategory);
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('subcategory_edit', array('id' => $subCategory->getId()));
+            return $this->redirectToRoute('subcategory_index', array('id' => $subCategory->getId()));
         }
 
         return $this->render('subcategory/edit.html.twig', array(
             'subCategory' => $subCategory,
             'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -94,31 +92,15 @@ class SubCategoryController extends Controller
      */
     public function deleteAction(Request $request, SubCategory $subCategory)
     {
-        $form = $this->createDeleteForm($subCategory);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
+//        $form = $this->createDeleteForm($subCategory);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($subCategory);
             $em->flush();
-        }
+//        }
 
         return $this->redirectToRoute('subcategory_index');
-    }
-
-    /**
-     * Creates a form to delete a subCategory entity.
-     *
-     * @param SubCategory $subCategory The subCategory entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(SubCategory $subCategory)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('subcategory_delete', array('id' => $subCategory->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
