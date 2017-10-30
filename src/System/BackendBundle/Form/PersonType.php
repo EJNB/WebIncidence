b632371d035f4850ab010734c62e3411a2d30d6c
Enio Javier Nieto Basnueva <javiernieto1989@gmail.com>
2017-10-29 21:56:38 -0400

    antes de hacer la creacion de el crud de claim

--- src/System/BackendBundle/Form/PersonType.php
+++ src/System/BackendBundle/Form/PersonType.php
@@ -3,9 +3,16 @@
 namespace System\BackendBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class PersonType extends AbstractType
 {
@@ -15,17 +22,42 @@ class PersonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('password',PasswordType::class)
-            ->add('name')
-            ->add('occupation')
-            ->add('isActive')
+            ->add('email',EmailType::class,array(
+                'label' => 'Correo'
+            ))
+            ->add('username',TextType::class,array(
+                'label' => 'Usuario'
+            ))
+//            'password',PasswordType::class
+            ->addEventListener(FormEvents::PRE_SET_DATA,function(FormEvent $event){
+                $person = $event->getData();//Returns the data associated with this event.
+                $form = $event->getForm();//Returns the form at the source of the event.
+                // check if the Person object is "new", If no data is passed to the form, the data is "null".
+                // This should be considered a new "Person"
+                if(!$person || null===$person->getId()){
+                    $form
+                        ->add('password',PasswordType::class)
+                        ->add('password',RepeatedType::class,array(
+                            'type' => PasswordType::class,
+                            'invalid_message' => 'Sus contraseñas no coincidir',
+                            'required' => true,
+                            'first_options' => array('label' => 'Contraseña'),
+                            'second_options' => array('label' => 'Repita la contraseña')
+                        ));
+                }
+            })
+            ->add('name',TextType::class,array(
+                'label' => 'Nombre y apellidos'
+            ))
+            ->add('occupation',TextType::class,array(
+                'label' => 'Cargo'
+            ))
+            ->add('isActive',CheckboxType::class)
             ->add('consultant')
             ->add('roles')
             ->add('department');
     }
-    
+
     /**
      * {@inheritdoc}
      */
